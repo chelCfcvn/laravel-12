c141d42a0944464aed722fff7488cb5a4dda2937
TuyenND <tuyennguyen06092001@gmail.com>
2025-02-26 13:23:32 +0700

    testing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-import './bootstrap';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+const pages = import.meta.glob('./Pages/**/*.vue');
+
+createInertiaApp({
+    resolve: name => {
+        const page = pages[`./Pages/${name}.vue`];
+        if (!page) {
+            throw new Error(`Page ${name} not found`);
+        }
+        return page();
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+});
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function () {
-    return view('hello');
+Route::get('/welcome', function () {
+    return Inertia::render('Welcome');
 });
